e81ab73d7eb26f29077f0e16d52e49174db3e0d6
Jamie Hollands <Hollands09@gmail.com>
2020-12-08 10:40:55 -0500

    test

--- Trie/Trie/Trie.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <boost/align.hpp>
-
-int main() {
-
-	std::cout << "This is just a test to get the solution started and have the boost library set up so we all dont have to mess around with it" << std::endl;
-
-	system("pause");
-}
