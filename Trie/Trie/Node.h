7d4114638133d922d01f553f8b9f2dee884c89f1
Jamie Hollands <Hollands09@gmail.com>
2020-12-08 10:19:33 -0500

    Trie and Dictionary update

---
+++ Trie/Trie/Node.h
@@ -0,0 +1,22 @@
+#ifndef _NODE_
+#define _NODE_
+
+class Node
+{
+public:
+
+	Node();
+	
+	Node *getNode(void);
+	
+	~Node();
+
+
+
+	//Member variable
+
+	bool endOfWord = false;
+
+};
+
+#endif // !_NODE_
