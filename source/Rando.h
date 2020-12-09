b2ba4c539c7514cc88c5fd7ed0ab0550f5380e72
Hollands09 <46874978+Hollands09@users.noreply.github.com>
2020-12-09 11:00:43 -0500

    Update Rando.h

--- source/Rando.h
+++ source/Rando.h
@@ -1,16 +1,20 @@
 #ifndef _RANDO_
 #define _RANDO_
-
+////////////////////////////////////////////////////////////
+//
+// This is just use for generating a seeded random number.
+//
+////////////////////////////////////////////////////////////
 #include <random>
 #include <chrono>
 
-// Random number generator for selecting a random word
 unsigned int rando(int max) {
+
+	unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();  //Time since epoch Doesnt like that is cast to an u_int but oh well.
 	
-	// Seed based off of system clock
-	unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
-	std::default_random_engine generator(seed);
-	std::uniform_int_distribution<int> dist(0, max);
+	std::default_random_engine generator(seed); //std library number generator c++11.  Can be seeded like we did here.
+	
+	std::uniform_int_distribution<int> dist(0, max); //c++11 goodies for making ensuring that the random generated number is "random"
 
 	seed = dist(generator);
 
