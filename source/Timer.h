f8d2cdae1921fb61d499749fcf93ef8d43af8327
Hollands09 <46874978+Hollands09@users.noreply.github.com>
2020-12-09 10:49:39 -0500

    Update Timer.h

--- source/Timer.h
+++ source/Timer.h
@@ -1,5 +1,10 @@
 #ifndef _TIMER_
 #define _TIMER_
+////////////////////////////////////////////////////////////
+//
+// Timer class for tracking and storing run time.
+//
+////////////////////////////////////////////////////////////
 
 #include <chrono>
 #include <mutex>
@@ -12,12 +17,20 @@ using namespace std::chrono;
 class Timer
 {
 	public:
-		
+		//default constructor
 		Timer();
+		
+		//Destructor
+		~Timer();
 
+		//pass 1 or 2 to set respective clocks.  If 2 is entered it will also output the run time.
 		void setTime(int clockNum);
 
+		//part of chrono declared here and initialized in constructor.
 		high_resolution_clock::time_point t1, t2;
+		
+		//Not a microsecond  value in the traditional sense, its still part of the chrono and if awful to cast
+		//Stores the delta between t2 and t1.
 		microseconds timeElapsed;
 };
 
