b367edd890921b9ffcfa838b5dab6cf556cd61b7
Hollands09 <46874978+Hollands09@users.noreply.github.com>
2020-12-09 10:50:12 -0500

    Update Trie.h

--- source/Trie.h
+++ source/Trie.h
@@ -1,36 +1,59 @@
 #ifndef _TRIE_
 #define _TRIE_
+////////////////////////////////////////////////////////////
+//
+// Implementation of a Trie.
+//
+//TODO: Add recursive delete.  Dotfile generations.
+////////////////////////////////////////////////////////////
+
 
 #include <string>
 #include <unordered_map>
-#include "Timer.h"
 #include <errno.h>
 #include <fstream>
+#include "Timer.h"
 
 class Trie
 {
 public:
 
+	//Default constructor nothing special here.
 	Trie();
 
+	//Destructor
+	~Trie();
+
+	//Creates new node and returns it.
 	Trie* createNode();
 
+	//passed pointer refernce more specifically to modify the map for childen
 	void insert(Trie*& root, std::string* key, std::ofstream &dotfile);
 
+	//Searches the trie for specified word, returns number of repeats.
 	int search(Trie*& root, std::string* key);
 
+	/*
+	I never want to look at these again but I will.
 	void triePrintNull(std::string key, int nullcount, std::ofstream& dotfile);
 
-	void dotPrintHelper(Trie*& node, std::string *key, std::ofstream& dotfile);
-
 	void triePrintDot(Trie*& node, std::ofstream& dotfile);
+	*/ 
 
+
+	void dotPrintHelper(Trie*& node, std::string* key, std::ofstream& dotfile);
+
+	//If leaf endString = true.
 	bool endString = false;
 	
+	//Used in prior implementations using arrays.
 	int depth = 0;
-
+	
+	//If a string is added twice it increments (string actually wont be in trie twice but it was tried to be added)
 	int count = 0;
 
+	//I didn't want to use maps at all but the larger inputs and the larger dictionaries were causing crashes due to the large amount 
+	//of unused pointers towards the end of the bottom of the tree.
 	std::unordered_map<char, Trie*> map;
 
 };
