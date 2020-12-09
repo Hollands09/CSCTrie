aca397a890e984d115597c2837be680acf024933
rlew130 <74322408+rlew130@users.noreply.github.com>
2020-12-09 10:17:15 -0500

    Update Rando.h

--- source/Rando.h
+++ source/Rando.h
@@ -4,8 +4,10 @@
 #include <random>
 #include <chrono>
 
+// Random number generator for selecting a random word
 unsigned int rando(int max) {
-
+	
+	// Seed based off of system clock
 	unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
 	std::default_random_engine generator(seed);
 	std::uniform_int_distribution<int> dist(0, max);
